daafb0ce2ca1fc164dce0f3e810e55cb145765d4
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-11-07 16:14:46 -0600

    First commit

---
+++ src/main/java/com/ama/springpracticeexercises/SpringPracticeExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.ama.springpracticeexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeExercisesApplication.class, args);
+    }
+
+}
