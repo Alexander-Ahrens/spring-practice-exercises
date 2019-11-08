253582ac56cbd0753d0ee080b3e64436ffe56e0e
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-11-08 17:04:46 -0600

    In a rush, not sure what changes I've made.

--- src/main/java/com/ama/springpracticeexercises/controllers/PostController.java
+++ src/main/java/com/ama/springpracticeexercises/controllers/PostController.java
@@ -1,29 +1,39 @@
 package com.ama.springpracticeexercises.controllers;
 
+import com.ama.springpracticeexercises.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller // Identifies class as a controller without having to create the POST and GET requests
 public class PostController {
 
     // Defines method that should be invoked by the URI, in this case, "posts"
+    // Shows all the posts
     @GetMapping("/posts")
     @ResponseBody // Is whats displays on the web page: "List of legen... wait for it! DARY posts"
     public String indexPage() {
         return "List of legen... wait for it! DARY posts";
     }
 
+
+    // Individual post
     @GetMapping("/posts/{id}")
     @ResponseBody
     public String postName(@PathVariable int id) {
+        ArrayList<Post> arrayList = new ArrayList<Post>();
+
         return "Selling: " + id;
     }
 
+    // Page that creates an individual post
     @GetMapping("/posts/create")
     @ResponseBody
     public String createPost() {
         return "Enter the name and description of your post.";
     }
+
 
     @PostMapping("posts/create")
     @ResponseBody
