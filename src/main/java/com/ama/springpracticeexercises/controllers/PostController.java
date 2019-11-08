fdc79efeeed6c0b41806cf900061eb804830811b
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-11-08 09:57:02 -0600

    Added indexPage, postName, createPost, and post methods into PostController

--- src/main/java/com/ama/springpracticeexercises/controllers/PostController.java
+++ src/main/java/com/ama/springpracticeexercises/controllers/PostController.java
@@ -1,7 +1,38 @@
 package com.ama.springpracticeexercises.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String indexPage() {
+        return "List of legen... wait for it! DARY posts";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postName() {
+        return "Selling: Superman powers";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "Enter the name and description of your post.";
+    }
+
+    @PostMapping("posts/create")
+    @ResponseBody
+    public String post(@RequestParam String title, @RequestParam String body) {
+        System.out.println("Title: " + title);
+        System.out.println("Body: " + body);
+        return "Create post";
+    }
+
 }
